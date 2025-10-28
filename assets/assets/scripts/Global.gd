78eb1cfcecb2827bd98e8d2a9dbf13926a656670
Kevin-Ashton-169 <bgamer169@gmail.com>
2025-10-28 21:00:39 +0000

    Delete assets/assets directory

--- assets/assets/scripts/Global.gd
+++
@@ -1,5 +0,0 @@
-extends Node
-
-var series_path = ""
-var chapter
-var media = "manhwa"
--- assets/assets/scripts/options_menu.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-@onready var top_bar : PackedScene = preload("res://assets/scenes/top_bar.tscn")
-
-func _ready():
-	var top_bar_temp = top_bar.instantiate()
-	add_child(top_bar_temp)
-	$Top_Bar.set_bar("options")
